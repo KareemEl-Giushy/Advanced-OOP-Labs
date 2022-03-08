3bfad83a05363613c3563560932828682f095184
Kareem El-Giushy <kareem.64123@gmail.com>
2022-03-08 18:17:00 +0200

    Linear Search Complete

---
+++ Linear Search/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Linear_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size");
+            byte size = byte.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+            for(int i = 0;i < arr.Length;i++) {
+                Console.WriteLine("Enter Element Number {0}:", i+1);
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("What Do you Search For");
+            int item = int.Parse(Console.ReadLine());
+            
+            if(LinearSearch(arr, item))
+                Console.WriteLine("Found !");
+            else
+                Console.WriteLine("Not Found :(");
+        }
+
+        public static bool LinearSearch(int[] arr, int item) {
+            foreach (int i in arr) {
+                if(i == item) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
