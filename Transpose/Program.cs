7c79ec632fdee544975e7d2259f2c9ede7681434
Kareem El-Giushy <kareem.64123@gmail.com>
2022-03-11 21:58:39 +0200

    Add Matrix Transpose

---
+++ Transpose/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Transpose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Rows: ");
+            int r = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter Columns: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matrix = new int[r, c];
+            int[,] matrixTranspose = new int[r, c];
+
+            Console.WriteLine("Enter Matrix Elements:");
+            for(int i = 0;i < matrix.GetLength(0);i++)
+                for(int j = 0;j < matrix.GetLength(1);j++)
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+
+            for(int i = 0;i < matrix.GetLength(0);i++)
+                for(int j = 0;j < matrix.GetLength(1);j++)
+                    matrixTranspose[j, i] = matrix[i, j];
+
+            Console.WriteLine("Matrix: ");
+            PrintMatrix(matrix);
+            Console.WriteLine("Transpose: ");
+            PrintMatrix(matrixTranspose);
+
+        }
+
+        static void PrintMatrix(int[,] m) {
+            for(int i = 0;i < m.GetLength(0);i++){
+                for(int j = 0;j < m.GetLength(1);j++)
+                    Console.Write(m[i, j] + " ");
+                
+                Console.WriteLine();
+            }
+        }
+    }
+}
